a098bc661082290c145dbe61a422df192a597822
blockche <blockche@qq.com>
2023-11-23 07:28:04 +0800

    更新部分注释

--- src/main/java/com/lxq/Interceptor/UrlInterceptor.java
+++ src/main/java/com/lxq/Interceptor/UrlInterceptor.java
@@ -10,12 +10,12 @@ public class UrlInterceptor implements HandlerInterceptor {
     //在业务处理器处理请求之前被调用。预处理，可以进行编码、安全控制、权限校验等处理；
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object arg2) throws Exception {
-//        System.out.println("Intercepor:preHandle被执行！！");
-//        System.out.println("preHandle:session1  "+request.getSession().getAttribute("users"));
+    //System.out.println("Intercepor:preHandle被执行！！");
+    //System.out.println("preHandle:session1  "+request.getSession().getAttribute("users"));
         //从Session中获取在线的用户
         Object user = request.getSession().getAttribute("users");
 
-//        System.out.println("preHandle:session2  "+request.getSession().getAttribute("users"));
+        //System.out.println("preHandle:session2  "+request.getSession().getAttribute("users"));
         String requestURI = request.getRequestURI();
         System.out.println("requestURI:"+requestURI);
 
@@ -25,14 +25,14 @@ public class UrlInterceptor implements HandlerInterceptor {
                 || "/user/outSignIn".equals(requestURI)
                 || "/error".equals(requestURI)
         ){
-//            System.out.println("请求放行！！");
+            //System.out.println("请求放行！！");
             return true;
         }
 
         //访问其他url需要判断是否登录
         if (user == null) {
             //请求转发
-//            request.getRequestDispatcher("/").forward(request, response);
+            //request.getRequestDispatcher("/").forward(request, response);
             //重定向
             response.sendRedirect("/");
             return false;
